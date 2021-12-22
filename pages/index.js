c63118a0af688df792acfa69d30c58aa32997568
promehedi <mmhasan563@gmail.com>
2021-12-23 02:37:33 +0600

    Update Home Campaign Card

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,25 @@
-import { Campaign, Layout } from '../components'
+import Link from 'next/link'
+import { Button, Card, Icon } from 'semantic-ui-react'
+import { Layout } from '../components'
 import { factoryInstance } from '../ethereum/instance'
 
 const Home = ({ campaigns }) => {
   return (
     <Layout title='Created Campaigns:'>
       {campaigns.map((campaign, index) => (
-        <Campaign key={index} data={campaign} />
+        <Card fluid key={index}>
+          <Card.Content>
+            <Card.Header>{campaign}</Card.Header>
+          </Card.Content>
+          <Card.Content>
+            <Link href={`/campaigns/${campaign}`} passHref>
+              <Button basic icon labelPosition='right' color='green'>
+                View Campaign
+                <Icon name='right arrow' />
+              </Button>
+            </Link>
+          </Card.Content>
+        </Card>
       ))}
     </Layout>
   )
