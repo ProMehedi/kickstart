5a23e337262deceae2e52c0dd6dc4a3d49c819b8
promehedi <mmhasan563@gmail.com>
2021-12-23 03:07:02 +0600

    Create cards for Campaign-single page

--- pages/campaigns/[address].js
+++ pages/campaigns/[address].js
@@ -2,25 +2,65 @@ import { Card } from 'semantic-ui-react'
 import { Layout } from '../../components'
 import { campaignInstance } from '../../ethereum'
 
-const Campaigns = ({ campaign }) => {
-  console.log(campaign)
+const Campaign = ({ campaign }) => {
   return (
     <Layout title='Single Campaing'>
-      <Card fluid>
-        <Card.Content>
-          <Card.Header>{campaign.manager}</Card.Header>
-          <Card.Meta>Address of Manager</Card.Meta>
-          <Card.Description>
-            The manager cretaed this campaign and can create requests to
-            withdraw money.
-          </Card.Description>
-        </Card.Content>
-      </Card>
+      <Card.Group itemsPerRow={2}>
+        <Card fluid>
+          <Card.Content>
+            <Card.Header style={{ lineBreak: 'anywhere' }}>
+              {campaign.manager}
+            </Card.Header>
+            <Card.Meta>Address of Manager</Card.Meta>
+            <Card.Description>
+              The manager cretaed this campaign and can create requests to
+              withdraw money.
+            </Card.Description>
+          </Card.Content>
+        </Card>
+        <Card fluid>
+          <Card.Content>
+            <Card.Header>{campaign.minContribute}</Card.Header>
+            <Card.Meta>Minimum Contribution(wei)</Card.Meta>
+            <Card.Description>
+              You must contribute at least this much wei to become an approver
+            </Card.Description>
+          </Card.Content>
+        </Card>
+        <Card fluid>
+          <Card.Content>
+            <Card.Header>{campaign.requestsCount}</Card.Header>
+            <Card.Meta>Number of Requests</Card.Meta>
+            <Card.Description>
+              A request tries to withdraw money from the contract. Requests must
+              be approved by approvers.
+            </Card.Description>
+          </Card.Content>
+        </Card>
+        <Card fluid>
+          <Card.Content>
+            <Card.Header>{campaign.approversCount}</Card.Header>
+            <Card.Meta>Number of Approvers</Card.Meta>
+            <Card.Description>
+              Number of people who have already donated to this campaign.
+            </Card.Description>
+          </Card.Content>
+        </Card>
+        <Card fluid>
+          <Card.Content>
+            <Card.Header>{campaign.balance}</Card.Header>
+            <Card.Meta>Campaign Balance</Card.Meta>
+            <Card.Description>
+              The balance is how much money this campaign has left to spend.
+            </Card.Description>
+          </Card.Content>
+        </Card>
+      </Card.Group>
     </Layout>
   )
 }
 
-export default Campaigns
+export default Campaign
 
 export const getServerSideProps = async ({ query }) => {
   const Campaign = campaignInstance(query.address)
