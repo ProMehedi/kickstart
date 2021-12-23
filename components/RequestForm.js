6b18748cdc9dbbba4793ffcfb01dea2023c9ccd4
promehedi <mmhasan563@gmail.com>
2021-12-24 02:18:01 +0600

    Create Reuqests

---
+++ components/RequestForm.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { Button, Form, Icon } from 'semantic-ui-react'
+
+const RequestForm = ({ onSubmit, loading }) => {
+  const [request, setRequest] = React.useState({
+    description: '',
+    value: '',
+    recipient: '',
+  })
+
+  const handleChange = (e) => {
+    setRequest({
+      ...request,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  return (
+    <Form onSubmit={() => onSubmit(request)}>
+      <Form.Field
+        control='input'
+        label='Description'
+        name='description'
+        placeholder='Description'
+        onChange={handleChange}
+      />
+      <Form.Field
+        control='input'
+        label='Recipient'
+        name='recipient'
+        placeholder='Recipient'
+        onChange={handleChange}
+      />
+      <Form.Field
+        control='input'
+        name='value'
+        label='Value in Ether'
+        type='number'
+        placeholder='Amount (Ether)'
+        step='any'
+        onChange={handleChange}
+      />
+      <Form.Field style={{ width: 'auto' }}>
+        <Button
+          icon
+          labelPosition='right'
+          color='green'
+          type='submit'
+          loading={loading}
+          disabled={loading}
+        >
+          Contribute
+          <Icon name='plus' />
+        </Button>
+      </Form.Field>
+    </Form>
+  )
+}
+
+export default RequestForm
