c6d53f3e04214b7b12f5219fae7eb86deab6aa49
promehedi <mmhasan563@gmail.com>
2021-12-24 03:11:46 +0600

    Create Requests Table

--- pages/campaigns/[address]/requests/index.js
+++ pages/campaigns/[address]/requests/index.js
@@ -1,18 +1,67 @@
 import Link from 'next/link'
 import React from 'react'
-import { Button, Icon } from 'semantic-ui-react'
+import { Button, Icon, Table } from 'semantic-ui-react'
 import { Layout } from '../../../../components'
-import { campaignInstance } from '../../../../ethereum'
+import { campaignInstance, web3 } from '../../../../ethereum'
 
-const Requests = ({ address, requests }) => {
+const Requests = ({ address, requests, approvers }) => {
+  console.log(approvers)
   return (
     <Layout>
       <h1>List of Requests</h1>
-      {requests.map((request, index) => (
-        <div key={index}>
-          <h3>{request.description}</h3>
-        </div>
-      ))}
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>ID</Table.HeaderCell>
+            <Table.HeaderCell>Description</Table.HeaderCell>
+            <Table.HeaderCell>Amount</Table.HeaderCell>
+            <Table.HeaderCell>Recipient</Table.HeaderCell>
+            <Table.HeaderCell>Approval Count</Table.HeaderCell>
+            <Table.HeaderCell>Approve</Table.HeaderCell>
+            <Table.HeaderCell>Finalize</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {requests.map((request) => (
+            <Table.Row key={request.id}>
+              <Table.Cell>{request.id + 1}</Table.Cell>
+              <Table.Cell>{request.description}</Table.Cell>
+              <Table.Cell>
+                {web3.utils.fromWei(request.value, 'ether') + ' ETH'}
+              </Table.Cell>
+              <Table.Cell>{request.recipient}</Table.Cell>
+              <Table.Cell>
+                {request.approvalCount} / {approvers}
+              </Table.Cell>
+              <Table.Cell>
+                <Button
+                  basic
+                  icon
+                  labelPosition='right'
+                  size='small'
+                  color='green'
+                  disabled={request.approvalCount > 0}
+                >
+                  <Icon name='checkmark' />
+                  Approve
+                </Button>
+              </Table.Cell>
+              <Table.Cell>
+                <Button
+                  basic
+                  icon
+                  labelPosition='right'
+                  size='small'
+                  color='red'
+                >
+                  <Icon name='x' />
+                  Finalize
+                </Button>
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
       <Link href={`/campaigns/${address}/requests/new`} passHref>
         <Button
           basic
@@ -44,6 +93,9 @@ export const getServerSideProps = async ({ query }) => {
     const requestObj = { ...request, id: i }
     requests.push(requestObj)
   }
+  const approversCount = await campaignInstance(address)
+    .methods.approversCount()
+    .call()
 
-  return { props: { address, requests } }
+  return { props: { address, requests, approvers: approversCount } }
 }
