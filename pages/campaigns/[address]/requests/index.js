8b766321e893dc91e75b9aae1a0d7e57dba3c236
promehedi <mmhasan563@gmail.com>
2021-12-24 02:23:01 +0600

    Create List of Requests page

--- pages/campaigns/[address]/requests/index.js
+++ pages/campaigns/[address]/requests/index.js
@@ -1,51 +1,16 @@
 import Link from 'next/link'
 import React from 'react'
 import { Button, Icon } from 'semantic-ui-react'
-import { InfoMessage, Layout, RequestForm } from '../../../../components'
-import { campaignInstance, web3 } from '../../../../ethereum'
+import { Layout } from '../../../../components'
 
 const Requests = ({ address }) => {
-  const [error, setError] = React.useState('')
-  const [loading, setLoading] = React.useState(false)
-
-  const makeRequest = async (request) => {
-    const { description, value, recipient } = request
-    try {
-      setLoading(true)
-      const accounts = await web3.eth.getAccounts()
-      await campaignInstance(address)
-        .methods.createRequest(
-          description,
-          web3.utils.toWei(value, 'ether'),
-          recipient
-        )
-        .send({
-          from: accounts[0],
-        })
-      setLoading(false)
-      setError('')
-    } catch (err) {
-      setError(err.message)
-      setLoading(false)
-    }
-  }
-
   return (
     <Layout>
-      <h1>Requests!</h1>
-      <RequestForm onSubmit={makeRequest} loading={loading} />
-      {error && <InfoMessage error={error} />}
-      <Link href={`/campaigns/${address}/requests`} passHref>
-        <Button
-          basic
-          icon
-          labelPosition='left'
-          size='large'
-          color='blue'
-          style={{ marginTop: 30 }}
-        >
-          Back to Requests
-          <Icon name='left arrow' />
+      <h1>List of Requests</h1>
+      <Link href={`/campaigns/${address}/requests/new`} passHref>
+        <Button basic icon labelPosition='right' color='blue' size='large'>
+          Create New Request
+          <Icon name='plus' />
         </Button>
       </Link>
     </Layout>
--- pages/campaigns/[address]/requests/new.js
+++ pages/campaigns/[address]/requests/new.js
@@ -1,9 +1,65 @@
-const NewRequests = () => {
+import Link from 'next/link'
+import React from 'react'
+import { Button, Icon } from 'semantic-ui-react'
+import { InfoMessage, Layout, RequestForm } from '../../../../components'
+import { campaignInstance, web3 } from '../../../../ethereum'
+
+const NewRequests = ({ address }) => {
+  const [error, setError] = React.useState('')
+  const [loading, setLoading] = React.useState(false)
+
+  const makeRequest = async (request) => {
+    const { description, value, recipient } = request
+    try {
+      setLoading(true)
+      const accounts = await web3.eth.getAccounts()
+      await campaignInstance(address)
+        .methods.createRequest(
+          description,
+          web3.utils.toWei(value, 'ether'),
+          recipient
+        )
+        .send({
+          from: accounts[0],
+        })
+      setLoading(false)
+      setError('')
+    } catch (err) {
+      setError(err.message)
+      setLoading(false)
+    }
+  }
+
   return (
-    <div>
-      <h1>New Requests!</h1>
-    </div>
+    <Layout>
+      <h1>Requests!</h1>
+      <RequestForm onSubmit={makeRequest} loading={loading} />
+      {error && <InfoMessage error={error} />}
+      <Link href={`/campaigns/${address}/requests`} passHref>
+        <Button
+          basic
+          icon
+          labelPosition='left'
+          size='large'
+          color='blue'
+          style={{ marginTop: 30 }}
+        >
+          Back to Requests
+          <Icon name='left arrow' />
+        </Button>
+      </Link>
+    </Layout>
   )
 }
 
 export default NewRequests
+
+export const getServerSideProps = ({ query }) => {
+  const address = query.address
+
+  return {
+    props: {
+      address,
+    },
+  }
+}
